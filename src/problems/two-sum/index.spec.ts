5251a5b9f4b986bd20744ac288ee70d30262f54f
czzonet <czzonet@163.com>
2020-07-28 21:25:50 +0800

    [feat] [two-sum] resolved 添加解法

--- src/problems/two-sum/index.spec.ts
+++ src/problems/two-sum/index.spec.ts
@@ -1,5 +1,8 @@
 import { twoSum } from ".";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(twoSum([2, 7, 11, 15])).toStrictEqual([0, 1]);
+test("[2, 7, 11, 15], 9 to equal [0, 1]", () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+});
+test("[3, 2, 4], 6 to equal [1, 2]", () => {
+  expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
 });
