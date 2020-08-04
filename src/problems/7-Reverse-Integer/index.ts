e38f575a111126aa66ba24c03159806d48d57c2f
czzonet <czzonet@163.com>
2020-08-04 18:38:58 +0800

    [feat] [7] ok 完成整数翻转

--- src/problems/7-Reverse-Integer/index.ts
+++ src/problems/7-Reverse-Integer/index.ts
@@ -1,3 +1,17 @@
-function reverse(x: number): number {
-  return 0;
+export function reverse(x: number): number {
+  const val = x > 0 ? x : -x;
+  const numsRaw = val.toString().split("");
+
+  numsRaw.reverse();
+
+  const strTarget = numsRaw.join("");
+
+  let target = x > 0 ? parseInt(strTarget) : -parseInt(strTarget);
+
+  let output = 0;
+  if (target <= Math.pow(2, 31) - 1 && target >= -Math.pow(2, 31)) {
+    output = target;
+  }
+
+  return output;
 }
