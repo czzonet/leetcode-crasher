af8ed0a80dda7784fd58f50728d391d8fa92e3ec
czzonet <czzonet@163.com>
2020-08-05 14:50:34 +0800

    [feat] [9] ok 完成回文判断

--- src/problems/9-Palindrome-Number/index.ts
+++ src/problems/9-Palindrome-Number/index.ts
@@ -1,3 +1,25 @@
-function isPalindrome(x: number): boolean {
-  return true;
+export function isPalindrome(x: number): boolean {
+  const numsRaw = x.toString().split("");
+
+  let isPalindrom = true;
+
+  const isOdd = numsRaw.length % 2 === 0;
+
+  if (isOdd) {
+    for (let index = 0; index < numsRaw.length / 2; index++) {
+      if (numsRaw[index] !== numsRaw[numsRaw.length - index - 1]) {
+        isPalindrom = false;
+        break;
+      }
+    }
+  } else {
+    for (let index = 0; index < (numsRaw.length + 1) / 2; index++) {
+      if (numsRaw[index] !== numsRaw[numsRaw.length - index - 1]) {
+        isPalindrom = false;
+        break;
+      }
+    }
+  }
+
+  return isPalindrom;
 }
