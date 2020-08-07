8fad6028b5d2d62ada78dae6d6de6e9d4aadcf42
czzonet <czzonet@163.com>
2020-08-07 21:56:38 +0800

    [feat] [10] ok 完成最大体积计算

--- src/problems/11-Container-With-Most-Water/index.ts
+++ src/problems/11-Container-With-Most-Water/index.ts
@@ -1,3 +1,22 @@
 export function maxArea(height: number[]): number {
-  return 0;
+  /** 似乎题目不难 可能有优化方法 */
+
+  /** 体积就是距离乘以较小值 */
+  let v = 0;
+  let iOfv = 0;
+  let jOfv = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    const element = height[i];
+
+    for (let j = i + 1; j < height.length; j++) {
+      const widthCurrent = j - i;
+      const heightCurrent = height[i] < height[j] ? height[i] : height[j];
+      const vCurrent = widthCurrent * heightCurrent;
+      /** 当比较大时 记录信息 */
+      vCurrent > v ? ([v, iOfv, jOfv] = [vCurrent, i, j]) : null;
+    }
+  }
+
+  return v;
 }
