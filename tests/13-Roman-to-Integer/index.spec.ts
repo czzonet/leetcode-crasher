b536c00b60943b6e064a6a5e86b354333b689d96
czzonet <czzonet@163.com>
2020-08-17 17:05:35 +0800

    [feat] [13] ok 完成罗马数字的转换 远不止简单难度

--- tests/13-Roman-to-Integer/index.spec.ts
+++ tests/13-Roman-to-Integer/index.spec.ts
@@ -6,3 +6,29 @@ Output: 3
 `, () => {
   expect(romanToInt("III")).toBe(3);
 });
+test(`
+Input: "IV"
+Output: 4
+`, () => {
+  expect(romanToInt("IV")).toBe(4);
+});
+test(`
+Input: "IX"
+Output: 9
+`, () => {
+  expect(romanToInt("IX")).toBe(9);
+});
+test(`
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+`, () => {
+  expect(romanToInt("LVIII")).toBe(58);
+});
+test(`
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+`, () => {
+  expect(romanToInt("MCMXCIV")).toBe(1994);
+});
