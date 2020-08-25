546b17f6c5325474b4e9727fbc0e6b58d2f9b5db
czzonet <czzonet@163.com>
2020-08-25 15:35:53 +0800

    [feat] [15] first ok 完成三数和为零 基础思路 但超时了 而且去重不高效

--- src/problems/15-3Sum/index.ts
+++ src/problems/15-3Sum/index.ts
@@ -1,1 +1,46 @@
-export function threeSum(nums: number[]): number[][] {}
+export function threeSum(nums: number[]): number[][] {
+  const resNums: number[][] = [];
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        const res = [nums[i], nums[j], nums[k]];
+        const sum = res.reduce((p, v) => p + v, 0);
+
+        if (sum === 0) {
+          resNums.push(res);
+        }
+      }
+    }
+  }
+  const uniqeNums: number[][] = [];
+
+  resNums.forEach((d) => {
+    const isExist = uniqeNums.findIndex((v) => isDuplicateTriplets(d, v));
+    if (isExist === -1) {
+      uniqeNums.push(d);
+    }
+  });
+
+  return uniqeNums;
+}
+
+export function isDuplicateTriplets(a: number[], b: number[]): boolean {
+  const arrA = a.slice();
+  const arrB = b.slice();
+
+  let res = true;
+
+  for (let i = 0; i < arrA.length; i++) {
+    const element = arrA[i];
+
+    const sameFirstIndex = arrB.findIndex((d) => d === element);
+
+    if (sameFirstIndex === -1) {
+      res = false;
+    } else {
+      arrB.splice(sameFirstIndex, 1);
+    }
+  }
+
+  return res;
+}
--- tests/15-3Sum/index.spec.ts
+++ tests/15-3Sum/index.spec.ts
@@ -1,4 +1,4 @@
-import { threeSum } from "../../src/problems/15-3Sum";
+import { threeSum, isDuplicateTriplets } from "../../src/problems/15-3Sum";
 
 test(`
 Given array nums = [-1, 0, 1, 2, -1, -4],
@@ -8,9 +8,23 @@ A solution set is:
   [-1, 0, 1],
   [-1, -1, 2]
 ]
+//
+same as:
+[
+  [-1, 0, 1],
+  [-1, 2 ,-1]
+]
 `, () => {
-  expect(threeSum([[-1, 0, 1, 2, -1, -4]])).toBe([
+  expect(threeSum([-1, 0, 1, 2, -1, -4])).toStrictEqual([
     [-1, 0, 1],
-    [-1, -1, 2],
+    [-1, 2, -1],
   ]);
+});
+test(`
+
+  [-1, 0, 1],
+[  0,-1,  1],
+
+`, () => {
+  expect(isDuplicateTriplets([-1, 0, 1], [0, -1, 1])).toBe(true);
 });
