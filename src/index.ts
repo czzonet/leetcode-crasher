acf3d8b4734c1b9621625d001b98a99deace4d38
czzonet <czzonet@163.com>
2021-02-12 14:53:12 +0800

    feat: add v4 -- use listnode 添加最快的版本4解法 遍历一次链表即可

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,8 @@
-import { addTwoNumbersOfArray } from "./problems/2-add-two-numbers/v3/index";
-import { ListNode } from "./problems/2-add-two-numbers/v3/ListNode";
+import { threeSum } from "./problems/15-3Sum";
+import { test2 } from "./problems/21-Merge-Two-Sorted-Lists";
+
 function main() {
-  console.log("Welcome!");
-
-  const a = [2, 4, 3];
-
-  const b = [5, 6, 4];
-  const c = [7, 0, 8];
-
-  const test = addTwoNumbersOfArray(a, b);
-  new ListNode();
+  test2();
   debugger;
 }
 
